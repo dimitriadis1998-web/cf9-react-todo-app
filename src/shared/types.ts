ec194f63bbd15c56578397e043253d256cd21b0b
ΤοΌνομάΣου <email@.com>
2026-07-09 19:24:12 +0300

    feat(ui): add Button shared component

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,3 +1,10 @@
 export interface LayoutProps {
     children : React.ReactNode
 }
+
+export type ButtonProps = {
+    onClick?: () => void;
+    disabled?: boolean;
+    label : string;
+    addClasses?: string;
+}
