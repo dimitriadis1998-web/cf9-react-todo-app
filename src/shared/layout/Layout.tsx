9001f16d85c2c78f3ca5e28a6b579288ac4d02be
ΤοΌνομάΣου <email@.com>
2026-07-09 18:14:23 +0300

    feat(ui): add Header , Footer and Layout shared components

---
+++ src/shared/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+import type {LayoutProps} from "../types.ts";
+
+
+
+
+const Layout = ({children}:LayoutProps) => {
+
+    return (
+        <>
+            <Header/>
+            <main className="container mx-auto min-h-[95ch] pt-36">{children}</main>
+            <Footer/>
+        </>
+    )
+}
+export default Layout;
